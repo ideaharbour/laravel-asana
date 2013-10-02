533f754088da53c5a0768721a94f47e8de049fe1
samsmo <samsmolenski@gmail.com>
2013-10-02 12:04:22 -0400

    initial commit

---
+++ src/Smo/Asana/AsanaServiceProvider.php
@@ -0,0 +1,49 @@
+<?php namespace Smo\Asana;
+
+use Illuminate\Support\ServiceProvider;
+
+class AsanaServiceProvider extends ServiceProvider {
+
+	/**
+	 * Indicates if loading of the provider is deferred.
+	 *
+	 * @var bool
+	 */
+	protected $defer = false;
+
+	/**
+	 * Bootstrap the application events.
+	 *
+	 * @return void
+	 */
+	public function boot()
+	{
+		$this->package('smo/asana');
+	}
+
+	/**
+	 * Register the service provider.
+	 *
+	 * @return void
+	 */
+	public function register()
+	{
+		//
+		$this->app['asana'] = $this->app->share(function($app)
+            {
+            	var_dump($app);
+                return new Asana;
+            });
+	}
+
+	/**
+	 * Get the services provided by the provider.
+	 *
+	 * @return array
+	 */
+	public function provides()
+	{
+		return array('asana');
+	}
+
+}
